5e8be4246006d12a0ab83aae84dfc49b9ed4124a
softmagnet <59001819+softmagnet@users.noreply.github.com>
2021-07-26 14:52:29 +0800

    env var

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import 'firebase/firestore'
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyA2B2mSqR7UiM6HLfzzSsnkt6R42Pmz7Bk",
-    authDomain: "zhiwei-porfolio.firebaseapp.com",
-    projectId: "zhiwei-porfolio",
-    storageBucket: "zhiwei-porfolio.appspot.com",
-    messagingSenderId: "122880779843",
-    appId: "1:122880779843:web:992827f7c96b62141656f6"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 });
 
 export const db = app.firestore()
